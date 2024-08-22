3634ce791ca8cac8ba534569cd7533cef1293bfb
Syed Asadullah Shah <135575639+iamasadshah@users.noreply.github.com>
2024-08-22 14:05:18 +0500

    build

--- src/app/components/Second.tsx
+++ src/app/components/Second.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const Second = () => {
@@ -7,7 +8,9 @@ const Second = () => {
       Modern-css-with-tailwind-second-edition <br /> & <br /> CSS Flex &
       Grid_Tailwind CSS by Shruti Balasa
       <div className="my-4 flex justify-center items-center">
-        <img
+        <Image
+          width={100}
+          height={100}
           className="rounded hover:saturate-150 shadow-2xl hover:scale-110 cursor-grab hover:transition-transform"
           src="/book.png"
           alt="Book image"
