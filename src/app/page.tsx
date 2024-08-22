5985ec0323cccfd6f6eee8da4f86ab432dedada5
Syed Asadullah Shah <135575639+iamasadshah@users.noreply.github.com>
2024-08-22 16:40:10 +0500

    meta tags added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import First from "./components/First";
 import Second from "./components/Second";
 import Third from "./components/Third";
@@ -5,6 +6,12 @@ import Third from "./components/Third";
 export default function Home() {
   return (
     <>
+      <Head>
+        <meta
+          name="tags"
+          content="tailwind Css practice by Asad Shah, tailwind by asad shah, iamasadshah"
+        />
+      </Head>
       <First />
       <Second />
       <Third />
