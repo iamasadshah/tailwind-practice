01dcdb30af790c3d600d36a7176332ccebbf2956
Syed Asadullah Shah <135575639+iamasadshah@users.noreply.github.com>
2024-08-22 16:31:40 +0500

    main page updated

--- src/app/components/Third.tsx
+++ src/app/components/Third.tsx
@@ -10,38 +10,38 @@ const Third = () => {
       <div className="container grid grid-cols-3 px-6 my-14 gap-10 ">
         <Link
           href={"/blog-post"}
-          className="practices font-bold text-center bg-yellow-400 rounded-lg py-6 hover:bg-green-500 hover:scale-110 transition duration-300"
+          className="practices font-bold text-center bg-yellow-400 rounded-lg py-6 px-1 hover:bg-green-500 hover:scale-110 transition duration-300"
         >
           Blog Post
         </Link>
         <Link
           href={"/our-services"}
-          className="practices text-center font-bold bg-yellow-400 rounded-lg py-6 hover:bg-green-500 hover:scale-110 transition hover:shadow-lg hover:shadow-slate-700 shadow-slate-800"
+          className="practices text-center font-bold bg-yellow-400 rounded-lg py-6 px-1 hover:bg-green-500 hover:scale-110 transition duration-300 hover:shadow-lg hover:shadow-slate-700 shadow-slate-800"
         >
           Services Page
         </Link>
 
         <Link
           href={"/pricing-plans"}
-          className="practices text-center font-bold bg-yellow-400 rounded-lg py-6 hover:bg-green-500 hover:scale-110 hover:transition-transform"
+          className="practices text-center font-bold bg-yellow-400 rounded-lg py-6 px-1 hover:bg-green-500 hover:scale-110 transition duration-300 hover:shadow-lg hover:shadow-slate-700 shadow-slate-800"
         >
           Pricing Plans
         </Link>
         <Link
           href={"/article-preview"}
-          className="practices text-center font-bold bg-yellow-400 rounded-lg py-6 hover:bg-green-500 hover:scale-110 hover:transition-transform"
+          className="practices text-center font-bold bg-yellow-400 rounded-lg py-6 px-1 hover:bg-green-500 hover:scale-110 transition duration-300 hover:shadow-lg hover:shadow-slate-700 shadow-slate-800"
         >
           Article Preview
         </Link>
         <Link
           href={"/fitness-report"}
-          className="practices text-center font-bold bg-yellow-400 rounded-lg py-6 hover:bg-green-500 hover:scale-110 hover:transition-transform"
+          className="practices text-center font-bold bg-yellow-400 rounded-lg py-6 px-1 hover:bg-green-500 hover:scale-110 transition duration-300 hover:shadow-lg hover:shadow-slate-700 shadow-slate-800"
         >
           Fitness Report
         </Link>
         <Link
           href={"/restaurant-menus"}
-          className="practices text-center font-bold bg-yellow-400 rounded-lg py-6 hover:bg-green-500 hover:scale-110 hover:transition-transform"
+          className="practices text-center font-bold bg-yellow-400 rounded-lg py-6 px-1 hover:bg-green-500 hover:scale-110 transition duration-300 hover:shadow-lg hover:shadow-slate-700 shadow-slate-800"
         >
           Restaurant Menus
         </Link>
