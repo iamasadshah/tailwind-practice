788665c4eefd5011922526965193d239363d286c
Syed Asadullah Shah <135575639+iamasadshah@users.noreply.github.com>
2024-08-25 18:36:30 +0500

    responsive design

--- src/app/(grid)/pricing-plans/page.tsx
+++ src/app/(grid)/pricing-plans/page.tsx
@@ -4,20 +4,20 @@ const Page = () => {
   return (
     <>
       <div
-        className="container grid grid-cols-3 gap-x-8 p-24 place-items-center
+        className="container grid grid-cols-1 gap-x-8 p-24 place-items-center gap-y-4 md:grid-cols-3
       "
       >
-        <div className="cursor-pointer hover:scale-110 hover:transition-transform hover:bg-yellow-400 border shadow-xl rounded-md p-8 pr-32 hover:text-white">
+        <div className="cursor-pointer hover:scale-110 hover:transition-transform hover:bg-yellow-400 border shadow-xl rounded-md p-8 pr-32 hover:text-white md:p-2 md:px-10 lg:py-8 lg:px-20">
           <h3 className="font-bold text-2xl">Standard</h3>
           <p>Monthly Plan</p>
           <h2 className="font-bold text-4xl mt-4">$25</h2>
         </div>
-        <div className="bg-pink-600 cursor-pointer hover:scale-110 hover:transition-transform hover:shadow-2xl hover:bg-green-600  hover:text-white shadow-xl rounded-md p-8 pr-32 ">
+        <div className="bg-pink-600 cursor-pointer hover:scale-110 hover:transition-transform hover:shadow-2xl hover:bg-green-600  hover:text-white shadow-xl rounded-md p-8 pr-32 md:p-2 md:px-10 lg:py-8 lg:px-20">
           <h3 className="font-bold text-2xl">Popular</h3>
           <p>Monthly Plan</p>
           <h2 className="font-bold text-4xl mt-4">$40</h2>
         </div>
-        <div className="border shadow-xl rounded-md p-8 pr-32 cursor-pointer hover:scale-110 hover:transition-transform hover:bg-yellow-400 hover:text-white">
+        <div className="border shadow-xl rounded-md p-8 pr-32 cursor-pointer hover:scale-110 hover:transition-transform hover:bg-yellow-400 hover:text-white md:p-2 md:px-10 lg:py-8 lg:px-20">
           <h3 className="font-bold text-2xl">Premium</h3>
           <p>Monthly Plan</p>
           <h2 className="font-bold text-4xl mt-4">$55</h2>
